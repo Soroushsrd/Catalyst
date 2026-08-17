8919e0889990abcbe3607fcc9e2017df55648c11
Soroushsrd <Sardashtisoroosh@gmail.com>
2026-08-17 20:27:00 +0330

    add(cpp): initial parts of C Preprocessor lexer

--- cpp/src/lexer.rs
+++ cpp/src/lexer.rs
@@ -1,3 +1,26 @@
+use std::ops::Range;
+
+use crate::{
+    errors::PreprocessorErr,
+    reader::{Pos, Reader},
+};
+
+type PPResult<T> = Result<T, PreprocessorErr>;
+
+#[derive(Clone)]
+pub struct PPToken {
+    pub kind: PPTokenType,
+    pub pos: Pos,
+    pub range: Range<usize>,
+}
+
+impl PPToken {
+    pub fn new(kind: PPTokenType, pos: Pos, range: Range<usize>) -> Self {
+        Self { kind, pos, range }
+    }
+}
+
+#[derive(Clone)]
 pub enum PPTokenType {
     /// any sequence of letters, digits, or underscores
     /// which begin with _ or a letter
@@ -11,7 +34,7 @@ pub enum PPTokenType {
     Number,
     /// String consts. anything as "..." counts as string const
     /// inside "...", a backslash is used to escape chars
-    StringLiteral(String),
+    StringLiteral,
     /// Char consts. anything as '...' counts as char const
     /// inside '...', a backslash is used to escape chars
     /// prefix forms include 'L', 'u', 'U', 'u8', and R"..."
@@ -101,4 +124,79 @@ pub enum Punct {
     Comma,    // ,
     Hash,     // #    also %:
     HashHash, // ##   also %:%:
+}
+
+pub struct Lexer<'a> {
+    reader: Reader<'a>,
+    tokens: Vec<PPToken>,
+    errors: Vec<PreprocessorErr>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(reader: Reader<'a>) -> Self {
+        Self {
+            reader,
+            tokens: Vec::new(),
+            errors: Vec::new(),
+        }
+    }
+
+    /// a high level parser. should call underlying parsing mechanisms
+    /// TODO: need one parser per PPTokenType
+    pub fn lex(&mut self) -> Vec<PPToken> {
+        while !self.reader.is_eof() {
+            self.scan_tokens();
+        }
+        let pos = self.reader.pos();
+        let range = Range {
+            start: self.reader.offset(),
+            end: self.reader.size(),
+        };
+        self.push_token(PPToken {
+            kind: PPTokenType::EOF,
+            pos,
+            range,
+        });
+        self.tokens.to_vec()
+    }
+
+    fn scan_tokens(&mut self) {
+        let c = self.advance();
+        match c {
+            b'[' => self.push_token(PPTokenType::Punc(Punct::LBracket), 1),
+            b']' => self.push_token(PPTokenType::Punc(Punct::RBracket), 1),
+        }
+    }
+    fn lex_ident(&mut self) {}
+    fn lex_number(&mut self) {}
+    fn lex_str_lit(&mut self) {}
+    fn lex_chr_lit(&mut self) {}
+    fn lex_header(&mut self) {}
+    fn lex_punc(&mut self) {}
+    fn lex_other(&mut self) {}
+
+    fn range(&self, token_length: usize) -> Range<usize> {
+        let start = self.reader.offset();
+        Range {
+            start,
+            end: start + token_length,
+        }
+    }
+    fn pos(&self) -> Pos {
+        self.reader.pos()
+    }
+    fn push_token(&mut self, kind: PPTokenType, token_length: usize) {
+        let token = PPToken::new(kind, self.pos(), self.range(token_length));
+        self.tokens.push(token);
+    }
+
+    fn advance(&mut self) -> u8 {
+        self.reader.advance()
+    }
+    fn peek(&self) -> u8 {
+        self.reader.peek()
+    }
+    fn peek_at(&self, n: usize) -> u8 {
+        self.reader.peek_at(n)
+    }
 }
